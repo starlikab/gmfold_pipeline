e679f87133dac1f73fd78c134e298eb15757201c
starlikab <148661175+starlikab@users.noreply.github.com>
2025-07-07 17:17:24 -0700

    Add files via upload

--- src/gmfold_pipeline/clustering.py
+++ src/gmfold_pipeline/clustering.py
@@ -1,5 +1,8 @@
 from sklearn.cluster import SpectralClustering
 import numpy as np
+import pandas as pd
+import hdbscan
+from sklearn.metrics import silhouette_score, calinski_harabasz_score, davies_bouldin_score
 
 def spect_clustering(X, n_clusters):
     # Run clustering
@@ -10,6 +13,34 @@ def spect_clustering(X, n_clusters):
     # Fit the model and predict cluster labels
     cluster_labels = spectral_clustering.fit_predict(X)
     return cluster_labels
+
+def display_results(results): 
+  df_results = pd.DataFrame(
+      results,
+      columns=["min_size", "eps", "min_sample", "n_clusters", "noise", "sil_score", "ch_score", "db_score"]
+  )
+  print(df_results.to_string(index=False))
+
+def hdbscan_scoring(topic):
+  results = []
+  for min_size in [3,5,8,10,15]:
+    for epsilon in [0.0, 0.1, 0.2]:
+      for min_sample in [1, 2, 3, None]:
+        clusterer = hdbscan.HDBSCAN(min_samples=min_sample, min_cluster_size=min_size, cluster_selection_epsilon = epsilon, metric='euclidean')  # adjust min_cluster_size as needed
+        labels = clusterer.fit_predict(topic)
+        n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+        n_noise = list(labels).count(-1)
+        # Compute silhouette score (excluding noise points)
+        if n_clusters > 1 and len(set(labels)) > 1:
+            mask = labels != -1
+            sil_score = silhouette_score(topic[mask], labels[mask])
+            ch_score = calinski_harabasz_score(topic[mask], labels[mask])
+            db_score = davies_bouldin_score(topic[mask], labels[mask])
+        else:
+            sil_score = float('nan')  # Not enough clusters to compute score
+        results.append((min_size, epsilon, min_sample, n_clusters, n_noise, sil_score, ch_score, db_score))
+  display_results(results)
+  return
 
 def overlaps(new_cluster_labels, spectral_cluster_labels, df):
 

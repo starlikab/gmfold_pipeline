a6d090a05bdfd686d7533d0d9b602501cacf45e5
starlikab <148661175+starlikab@users.noreply.github.com>
2025-07-08 15:22:51 -0700

    Update clustering.py

--- src/gmfold_pipeline/clustering.py
+++ src/gmfold_pipeline/clustering.py
@@ -14,6 +14,55 @@ def spect_clustering(X, n_clusters):
     cluster_labels = spectral_clustering.fit_predict(X)
     return cluster_labels
 
+def evaluate_spectral_clustering(X, cluster_range):
+    sil_scores = []
+    ch_scores = []
+    db_scores = []
+
+    for n_clusters in cluster_range:
+        sc = SpectralClustering(n_clusters=n_clusters, affinity='nearest_neighbors', random_state=42)
+        labels = sc.fit_predict(X)
+
+        # Only compute silhouette if n_clusters > 1
+        if len(set(labels)) > 1:
+            sil_scores.append(silhouette_score(X, labels))
+            ch_scores.append(calinski_harabasz_score(X, labels))
+            db_scores.append(davies_bouldin_score(X, labels))
+        else:
+            sil_scores.append(None)
+            ch_scores.append(None)
+            db_scores.append(None)
+
+    # return silhouette_scores, ch_scores, db_scores
+
+    # Range of clusters to test
+    # cluster_range = range(5, 75, 5)
+    # sil_scores, ch_scores, db_scores = evaluate_spectral_clustering(kmer_topic, cluster_range)
+
+    # Plot the results
+    plt.figure(figsize=(12, 4))
+
+    plt.subplot(1, 3, 1)
+    # plt.ylim(-0.3, 0.15) # set y-lims for consistency when re-runnning plots with different # of topics
+    plt.plot(cluster_range, sil_scores, marker='o')
+    plt.title('Silhouette Score')
+    plt.xlabel('Number of Clusters')
+
+    plt.subplot(1, 3, 2)
+    # plt.ylim(0, 128)
+    plt.plot(cluster_range, ch_scores, marker='o', color='green')
+    plt.title('Calinski-Harabasz Score')
+    plt.xlabel('Number of Clusters')
+
+    plt.subplot(1, 3, 3)
+    # plt.ylim(1, 2.9)
+    plt.plot(cluster_range, db_scores, marker='o', color='red')
+    plt.title('Davies-Bouldin Score')
+    plt.xlabel('Number of Clusters')
+
+    plt.tight_layout()
+    plt.show()
+
 def display_results(results): 
   df_results = pd.DataFrame(
       results,
